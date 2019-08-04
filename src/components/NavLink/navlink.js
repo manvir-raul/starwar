5f1c231e9a3a5604e65b02f290953f6c4a6f858c
manvirraul <manvirsinghraul@gmail.com>
2019-08-04 18:51:15 +0530

    before next and previous button

---
+++ src/components/NavLink/navlink.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./navlink.css";
+
+const NavLink = () => {
+  return (
+    <div>
+      <h1>star wars</h1>
+    </div>
+  );
+};
+
+export default NavLink;
